8fb92421456e02b1da694673ad451b02406aa1db
Rahma023 <rahmafaizyusuf@gmail.com>
2022-01-10 10:48:25 +0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home:HomePage()
+    home:HomePage(),
+    routes: <String,WidgetBuilder>{
+      "/SecondPage":(BuildContext context)=> SecondPage()
+
+
+    }
   ));
 }
   class HomePage extends StatelessWidget{
@@ -11,24 +16,56 @@ void main() {
       return Scaffold(
         appBar: AppBar(title: Text("Home"),backgroundColor: Colors.amberAccent),
         body: Container(
-          child: Center,
+          child: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: const <Widget>[
               IconButton(
                 icon: Icon(Icons.favorite,color: Colors.blue),
                 iconSize: 70.0,
-                onPressed: null,
-              ),
+                onPressed: 
+               //(){Navigator.of(context).pushNamed("/SecondPage");}
+              null),
               Text("Home")
             ]
           )
         )
-      
+        )
           );
         
       
     }
     
   }
+
+
+class SecondPage extends StatelessWidget{
+    @override
+    Widget build(BuildContext context){
+      return Scaffold(
+        appBar: AppBar(title: Text("Second Page"),backgroundColor: Colors.amberAccent),
+        body: Container(
+          child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              IconButton(
+                icon: Icon(Icons.home,color: Colors.indigo),
+                iconSize: 70.0,
+                onPressed: null,
+              ),
+              Text("SecondPage")
+            ]
+          )
+        )
+        )
+          );
+        
+      
+    }
+    
+  }
+
+
 
 
